f713a4a73dde2a8ea5d50c80642cf7824e48890b
zeeun <98653814+zeeun@users.noreply.github.com>
2023-07-23 05:52:36 +0900

    [feat]가중치누적하는중

--- src/components/QnaComponent.jsx
+++ src/components/QnaComponent.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+
 import "./teststyle.css";
 
 const qnaList = [
@@ -8,13 +10,24 @@ const qnaList = [
       { answer: "1", type: null },
       { answer: "2", type: null },
       { answer: "3", type: null },
-      { answer: "4", type: [1] },
-      { answer: "5", type: [1] },
-      { answer: "6", type: [1] },
+      { answer: "4", type: [0] },
+      { answer: "5", type: [0] },
+      { answer: "6", type: [0] },
     ],
   },
   {
     q: "2. 날 사랑하는 사람이 다른 사람을 더 좋아하게 되어 나를 떠날까봐 굉장히 걱정한다. ",
+    a: [
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [0] },
+      { answer: "5", type: [0] },
+      { answer: "6", type: [0] },
+    ],
+  },
+  {
+    q: "3. 사람들의 궁극적인 목적이 무엇인지 경계하는 편이다.",
     a: [
       { answer: "1", type: null },
       { answer: "2", type: null },
@@ -25,226 +38,213 @@ const qnaList = [
     ],
   },
   {
-    q: "3. 사람들의 궁극적인 목적이 무엇인지 경계하는 편이다.",
-    a: [
-      { answer: "1" },
-      {
-        answer: "2",
-      },
-      { answer: "3" },
-      { answer: "4", type: [2] },
-      { answer: "5", type: [2] },
-      { answer: "6", type: [2] },
-    ],
-  },
-  {
     q: "4. 사람들이 나를 해치지 않을까 하는 걱정에 경계를 늦출 수가 없다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [2] },
-      { answer: "5", type: [2] },
-      { answer: "6", type: [2] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [1] },
+      { answer: "5", type: [1] },
+      { answer: "6", type: [1] },
     ],
   },
   {
     q: "5. 보통 사람들 보다 병에 걸리거나 다른 나쁜 일이 내게 닥칠까봐 더 많이 걱정하는 편이다.",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [3] },
-      { answer: "5", type: [3] },
-      { answer: "6", type: [3] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [2] },
+      { answer: "5", type: [2] },
+      { answer: "6", type: [2] },
     ],
   },
   {
     q: "6. 파산해서 거지가 되거나 남에게 의탁하게 될까 봐 걱정한다.",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [2] },
+      { answer: "5", type: [2] },
+      { answer: "6", type: [2] },
+    ],
+  },
+  {
+    q: "7. 살아가는 동안 혼자 힘으로 난관을 극복해 나갈 수 가 없기에 도움을 줄 사람이 필요하다.",
+    a: [
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
       { answer: "4", type: [3] },
       { answer: "5", type: [3] },
       { answer: "6", type: [3] },
     ],
   },
   {
-    q: "7. 살아가는 동안 혼자 힘으로 난관을 극복해 나갈 수 가 없기에 도움을 줄 사람이 필요하다.",
-    a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [4] },
-      { answer: "5", type: [4] },
-      { answer: "6", type: [4] },
-    ],
-  },
-  {
     q: "8. 부모님과 나는 서로의 사생활에 지나치게 관여하는 경향이 있다.",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [4] },
-      { answer: "5", type: [4] },
-      { answer: "6", type: [4] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [3] },
+      { answer: "5", type: [3] },
+      { answer: "6", type: [3] },
     ],
   },
   {
     q: "9. 나를 돌봐주거나 나와 마음을 나누거나 내게 일어나는 일에 대해 깊이 염려해줄 사람이 없었다.",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [5] },
-      { answer: "5", type: [5] },
-      { answer: "6", type: [5] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [4] },
+      { answer: "5", type: [4] },
+      { answer: "6", type: [4] },
     ],
   },
   {
     q: "10. 이해와 공감, 지도, 충고, 지지에 대한 나의 정서적 욕구를 사람들이 만족시켜 준 적이 없다.  ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [4] },
+      { answer: "5", type: [4] },
+      { answer: "6", type: [4] },
+    ],
+  },
+  {
+    q: "11. 나는 소속감이 없다. 나는 남들과 다르고 어디에도 어울리지 않는다. ",
+    a: [
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
       { answer: "4", type: [5] },
       { answer: "5", type: [5] },
       { answer: "6", type: [5] },
     ],
   },
   {
-    q: "11. 나는 소속감이 없다. 나는 남들과 다르고 어디에도 어울리지 않는다. ",
-    a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [6] },
-      { answer: "5", type: [6] },
-      { answer: "6", type: [6] },
-    ],
-  },
-  {
     q: "12. 나는 따분하고 싫증나는 사람이다. 사교적인 자리에서 어떻게 이야기해야 할지 모르겠다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [6] },
-      { answer: "5", type: [6] },
-      { answer: "6", type: [6] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [5] },
+      { answer: "5", type: [5] },
+      { answer: "6", type: [5] },
     ],
   },
   {
     q: "13. 내가 원하는 사람이 나의 모든 진실을 알게 되면 나를 사랑할 수 없을 것이다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [7] },
-      { answer: "5", type: [7] },
-      { answer: "6", type: [7] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [6] },
+      { answer: "5", type: [6] },
+      { answer: "6", type: [6] },
     ],
   },
 
   {
     q: "14. 나는 다른 사람들의 사랑과 관심, 존경을 받을 가치가 없다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [6] },
+      { answer: "5", type: [6] },
+      { answer: "6", type: [6] },
+    ],
+  },
+  {
+    q: "15. 나는 일(학업)에 있어서 남들보다 능력이 없다.",
+    a: [
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
       { answer: "4", type: [7] },
       { answer: "5", type: [7] },
       { answer: "6", type: [7] },
     ],
   },
   {
-    q: "15. 나는 일(학업)에 있어서 남들보다 능력이 없다.",
-    a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [8] },
-      { answer: "5", type: [8] },
-      { answer: "6", type: [8] },
-    ],
-  },
-  {
     q: "16. 남들보다 재능이나 지적능력, 경력이 모자라기 때문에 나는 이 자리에 어울리지 않는다고 느낀다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [8] },
-      { answer: "5", type: [8] },
-      { answer: "6", type: [8] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [7] },
+      { answer: "5", type: [7] },
+      { answer: "6", type: [7] },
     ],
   },
   {
     q: "17. 나는 다른 사람들이 원하는 대로 해줄수밖에 없다. 그렇게 하지 않으면 사람들은 나에게 어떤 방식으로든 보복하거나 나를 거부할것이다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [9] },
-      { answer: "5", type: [9] },
-      { answer: "6", type: [9] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [8] },
+      { answer: "5", type: [8] },
+      { answer: "6", type: [8] },
     ],
   },
   {
     q: "18. 사람들은 내가 남들만을 위하고 자신을 위할줄 모른다고 생각한다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [8] },
+      { answer: "5", type: [8] },
+      { answer: "6", type: [8] },
+    ],
+  },
+  {
+    q: "19. 나는 최선을 다한다. 적당한 수준에 만족할 수 없다.  ",
+    a: [
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
       { answer: "4", type: [9] },
       { answer: "5", type: [9] },
       { answer: "6", type: [9] },
     ],
   },
   {
-    q: "19. 나는 최선을 다한다. 적당한 수준에 만족할 수 없다.  ",
-    a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [10] },
-      { answer: "5", type: [10] },
-      { answer: "6", type: [10] },
-    ],
-  },
-  {
     q: "20. 나는 할 일이 너무 많아서 쉬거나 즐길 시간이 없다. ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [10] },
-      { answer: "5", type: [10] },
-      { answer: "6", type: [10] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [9] },
+      { answer: "5", type: [9] },
+      { answer: "6", type: [9] },
     ],
   },
   {
     q: "21. 다른 사람이 지키는 정상적인 규칙이나 관례를 따를 필요는 없다.  ",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [11] },
-      { answer: "5", type: [11] },
-      { answer: "6", type: [11] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [10] },
+      { answer: "5", type: [10] },
+      { answer: "6", type: [10] },
     ],
   },
   {
     q: "22. 나는 일상적이고 지루한 일들을 완수해 내거나 내가 내 감정을 조절하는 습관을 기르지 못했다.",
     a: [
-      { answer: "1" },
-      { answer: "2" },
-      { answer: "3" },
-      { answer: "4", type: [11] },
-      { answer: "5", type: [11] },
-      { answer: "6", type: [11] },
+      { answer: "1", type: null },
+      { answer: "2", type: null },
+      { answer: "3", type: null },
+      { answer: "4", type: [10] },
+      { answer: "5", type: [10] },
+      { answer: "6", type: [10] },
     ],
   },
 ];
@@ -302,8 +302,13 @@ const QnaComponent = () => {
   const [qIdx, setQIdx] = useState(0);
 
   const calResult = () => {
-    console.log(select);
-    const result = select.indexOf(Math.max(...select));
+    console.log("final select:", select);
+    const max = Math.max(...select);
+    const result = select.reduce((acc, cur, idx) => {
+      if (cur === max) acc.push(idx);
+      return acc;
+    }, []);
+    console.log("result", result);
     return result;
   };
 
@@ -344,41 +349,26 @@ const QnaComponent = () => {
   };
 
   const addAnswer = (answerText, qIdx, idx) => {
-    const a = document.querySelector(".answerBox");
-    const answer = document.createElement("button");
-    answer.classList.add("answerList");
-    answer.classList.add("my-3");
-    answer.classList.add("py-3");
-    answer.classList.add("mx-auto");
-    answer.classList.add("fadeIn");
-
-    a.appendChild(answer);
-    answer.innerHTML = answerText;
-
-    answer.addEventListener(
-      "click",
-      () => {
-        const children = document.querySelectorAll(".answerList");
-        for (let i = 0; i < children.length; i++) {
-          children[i].disabled = true;
-          children[i].style.WebkitAnimation = "fadeOut 0.5s";
-          children[i].style.animation = "fadeOut 0.5s";
+    const answerClickHandler = () => {
+      const target = qnaList[qIdx].a[idx].type;
+      const updatedSelect = [...select];
+      if (target !== null) {
+        for (let i = 0; i < target.length; i++) {
+          updatedSelect[target[i]] += 1;
         }
-        setTimeout(() => {
-          const target = qnaList[qIdx].a[idx].type;
-          const updatedSelect = [...select];
-          for (let i = 0; i < target.length; i++) {
-            updatedSelect[target[i]] += 1;
-          }
-          setSelect(updatedSelect);
+      }
+      setSelect(updatedSelect);
+      console.log("Updated Select:", updatedSelect);
+      goNext(qIdx + 1);
+    };
 
-          for (let i = 0; i < children.length; i++) {
-            children[i].style.display = "none";
-          }
-          goNext(qIdx + 1);
-        }, 450);
-      },
-      false
+    return (
+      <button
+        className="answerList my-3 py-3 mx-auto fadeIn"
+        onClick={answerClickHandler}
+      >
+        {answerText}
+      </button>
     );
   };
 
@@ -390,13 +380,29 @@ const QnaComponent = () => {
 
     const q = document.querySelector(".qBox");
     q.innerHTML = qnaList[qIdx].q;
-    for (let i in qnaList[qIdx].a) {
-      addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
-    }
+
+    // Use map to render each answer with the AnswerComponent
+    const answers = qnaList[qIdx].a.map((answer, idx) => (
+      <AnswerComponent
+        key={idx}
+        answerText={answer.answer}
+        qIdx={qIdx}
+        idx={idx}
+      />
+    ));
+
+     const rootElement = document.querySelector(".answerBox");
+    const root = createRoot(rootElement);
+    root.render(answers);
+
     const status = document.querySelector(".statusBar");
     status.style.width = `${(100 / endPoint) * (qIdx + 1)}%`;
 
     setQIdx(qIdx);
+  };
+
+  const AnswerComponent = ({ answerText, qIdx, idx }) => {
+    return addAnswer(answerText, qIdx, idx);
   };
 
   const begin = () => {
@@ -418,6 +424,166 @@ const QnaComponent = () => {
       goNext(0);
     }, 450);
   };
+
+  // const QnaComponent = () => {
+  //   const endPoint = 22;
+  //   const [select, setSelect] = useState(Array(endPoint).fill(0));
+  //   const [qIdx, setQIdx] = useState(0);
+
+  //   const calResult = () => {
+  //     console.log("final select:", select);
+  //     const max = Math.max(...select);
+  //     const result = select.reduce((acc, cur, idx) => {
+  //       if (cur === max) acc.push(idx);
+  //       return acc;
+  //     }, []);
+  //     console.log("result", result);
+  //     return result;
+  //   };
+
+  //   //   const calResult = () => {
+  //   //     console.log("final select:" ,select);
+  //   //     const result = select.indexOf(Math.max(...select));
+  //   //       return result;
+  //   //   };
+
+  //   const setResult = () => {
+  //     const point = calResult();
+  //     const resultName = document.querySelector(".resultname");
+  //     resultName.innerHTML = infoList[point].name;
+
+  //     const resultImg = document.createElement("img");
+  //     const imgDiv = document.querySelector("#resultImg");
+  //     const imgURL = `img/image-${point}.png`;
+  //     resultImg.src = imgURL;
+  //     resultImg.alt = point;
+  //     resultImg.classList.add("img-fluid");
+  //     imgDiv.appendChild(resultImg);
+
+  //     const resultDesc = document.querySelector(".resultDesc");
+  //     resultDesc.innerHTML = infoList[point].desc;
+  //   };
+
+  //   const goResult = () => {
+  //     const qna = document.querySelector("#qna");
+  //     const result = document.querySelector("#result");
+
+  //     qna.style.WebkitAnimation = "fadeOut 1s";
+  //     qna.style.animation = "fadeOut 1s";
+
+  //     setTimeout(() => {
+  //       result.style.WebkitAnimation = "fadeIn 1s";
+  //       result.style.animation = "fadeIn 1s";
+
+  //       setTimeout(() => {
+  //         qna.style.display = "none";
+  //         result.style.display = "block";
+  //       }, 450);
+  //       setResult();
+  //     }, 450);
+  //     };
+
+  // const addAnswer = (answerText, qIdx, idx) => {
+  //   const answerClickHandler = () => {
+  //     const target = qnaList[qIdx].a[idx].type;
+  //     const updatedSelect = [...select];
+  //     if (target !== null) {
+  //       for (let i = 0; i < target.length; i++) {
+  //         updatedSelect[target[i]] += 1;
+  //       }
+  //     }
+  //     setSelect(updatedSelect);
+  //     console.log("Updated Select:", updatedSelect);
+  //     goNext(qIdx + 1);
+  //   };
+
+  //   return (
+  //     <button
+  //       className="answerList my-3 py-3 mx-auto fadeIn"
+  //       onClick={answerClickHandler}
+  //     >
+  //       {answerText}
+  //     </button>
+  //   );
+  // };
+
+  // //   const addAnswer = (answerText, qIdx, idx) => {
+  // //     const a = document.querySelector(".answerBox");
+  // //     const answer = document.createElement("button");
+  // //     answer.classList.add("answerList");
+  // //     answer.classList.add("my-3");
+  // //     answer.classList.add("py-3");
+  // //     answer.classList.add("mx-auto");
+  // //     answer.classList.add("fadeIn");
+
+  // //     a.appendChild(answer);
+  // //     answer.innerHTML = answerText;
+
+  // //     answer.addEventListener(
+  // //       "click",
+  // //       () => {
+  // //         const children = document.querySelectorAll(".answerList");
+  // //         for (let i = 0; i < children.length; i++) {
+  // //           children[i].disabled = true;
+  // //           children[i].style.WebkitAnimation = "fadeOut 0.5s";
+  // //           children[i].style.animation = "fadeOut 0.5s";
+  // //         }
+  // //         setTimeout(() => {
+  // //           const target = qnaList[qIdx].a[idx].type;
+  // //           const updatedSelect = [...select];
+  // //           if (target !== null) {
+  // //             for (let i = 0; i < target.length; i++) {
+  // //               updatedSelect[target[i]] += 1;
+  // //             }
+  // //           }
+  // //           setSelect(updatedSelect);
+  // //           console.log("Updated Select:", updatedSelect);
+  // //           for (let i = 0; i < children.length; i++) {
+  // //             children[i].style.display = "none";
+  // //           }
+  // //           goNext(qIdx + 1);
+  // //         }, 450);
+  // //       },
+  // //       false
+  // //     );
+  // //   };
+
+  //   const goNext = (qIdx) => {
+  //     if (qIdx === endPoint) {
+  //       goResult();
+  //       return;
+  //     }
+
+  //     const q = document.querySelector(".qBox");
+  //     q.innerHTML = qnaList[qIdx].q;
+  //     for (let i in qnaList[qIdx].a) {
+  //       addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+  //     }
+  //     const status = document.querySelector(".statusBar");
+  //     status.style.width = `${(100 / endPoint) * (qIdx + 1)}%`;
+
+  //     setQIdx(qIdx);
+  //   };
+
+  //   const begin = () => {
+  //     const main = document.querySelector("#main");
+  //     const qna = document.querySelector("#qna");
+
+  //     main.style.WebkitAnimation = "fadeOut 1s";
+  //     main.style.animation = "fadeOut 1s";
+
+  //     setTimeout(() => {
+  //       qna.style.WebkitAnimation = "fadeIn 1s";
+  //       qna.style.animation = "fadeIn 1s";
+
+  //       setTimeout(() => {
+  //         main.style.display = "none";
+  //         qna.style.display = "block";
+  //       }, 450);
+
+  //       goNext(0);
+  //     }, 450);
+  //   };
 
   return (
     <div className="container">
