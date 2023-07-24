aae8b42f7690eb49e586503fe813ef883612b00e
hyunseo <loveopop0617@gmail.com>
2023-07-25 04:52:34 +0900

    마음일지 UI 수정
    바보현서의작업물...

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,50 +11,39 @@ const Services = (e) => {
 
   return (
     <header className="masthead">
+      {/* 마음일지 */}
       <hr className="sign-divider-light"/>
       <h2 className="text-center mt-0 text-white text-center">마음일지</h2>
       <hr className="sign-divider-light"/>
-      <div class="s-grid">
-          {/*<div className="left-sidebar">
-              <ul className="sidebar-menu">
-                <li>Calendar</li>
-                <li>Diarys</li>
-                <li>Results</li>
-              </ul>
-            </div>*/}
-        <div class = "content">
-          <div class="diary">
-            <div>
-              <input 
-              name="DiaryTitle" 
-              placeholder="일기제목을 작성해주세요" 
-              />
-            </div>
-            <br></br>
+
+      <div>
+        <div className = "content">
+          {/* 일기 작성란 */}
+          <div className="diary">
+            <div className="date">{selectedDate.toDateString()}</div>
+
+            <input 
+            name="DiaryTitle" 
+            placeholder="일기제목을 작성해주세요" 
+            className="input mb-2"
+            />
             <textarea 
               name="postcontent"
               placeholder="오늘의 일기를 작성하세요..."
+              className="write mb-3"
             />
+            <div className="d-grid"><button className="btn btn-primary btn-xl">진단하기</button></div>
           </div>
-          <div class="d-grid">
-            <button className="btn btn-primary btn-xl">
-              진단하기
-            </button>
-          </div>
-        </div>
-        <br></br>
-        <div class="c-grid">
-          <div class="diary-calendar">
-            <Calendar
-            onChange={handleDateChange}
-            value={selectedDate}
-            />
-          </div> 
-          <p>{selectedDate.toDateString()}</p>
-          <div class="d-grid">
-            <button className="btn btn-primary btn-xl">
-              일기 작성
-            </button>
+
+          {/* 달력 */}
+
+          <div className="content">
+            <div className="mt-6 diary-calendar">
+              <Calendar
+              onChange={handleDateChange}
+              value={selectedDate}
+              />
+            </div>
           </div>
         </div>
       </div>
