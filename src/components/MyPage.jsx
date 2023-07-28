020c094a9a11a39ad1fd43bdcf1c6bea4df5c61f
C2hazelnut <c2yjcs03@sookmyung.ac.kr>
2023-07-28 22:00:57 +0900

    마이페이지에서 자신의 덫

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -19,9 +19,17 @@ function MyPage(){
             <hr class="sign-divider-light"/>
             <div class="container">
                 <div className="col-auto">
-                    <Sidebar />
                 </div>
                 <div class="mypagecontent">
+                    <div class="testresult">
+                        당신은 지금 "버림받음의 덫"에 걸려있습니다.<br/>
+                        <hr class="sign-divider-light"/>
+                        버림받음의 덫은<br/>
+                        사랑하는 사람이 자신을 떠나고,<br/>
+                        자신은 영원히 정서적으로 고립되어<br/> 
+                        살게 될 것이라는 느낌에 사로잡혀 있음을<br/> 
+                        의미합니다.
+                    </div>
                     <div className="mt-6 diary-calendar">
                         <Calendar
                         onChange={handleDateChange}
